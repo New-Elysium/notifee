e2bb22fe37c85eb704a53c2b8e6309d90443f951
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2026-03-17 20:12:20 +0530

    Minor fixups

--- packages/react-native/react-native.config.js
+++ packages/react-native/react-native.config.js
@@ -2,6 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
+        packageName: 'io.invertase.notifee',
         packageImportPath: 'import io.invertase.notifee.NotifeePackage;',
         packageInstance: 'new NotifeePackage()',
       },
